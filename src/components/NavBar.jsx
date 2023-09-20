6c14fe48f7f5be4e1476a9080d0c1cf36c70e19f
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-09-20 18:11:11 -0400

    made changes to home element

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll";
@@ -31,7 +32,9 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Pallavee</h1>
+        <Link to="home" smooth duration={600}>
+          <h1 className="text-5xl font-signature ml-2 cursor-pointer">Pallavee</h1>
+        </Link>
       </div>
 
       <ul className="hidden md:flex">
